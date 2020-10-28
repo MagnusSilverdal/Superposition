8be0445bd90832e858a037a453f9559552f5c889
MagnusSilverdal <magnus.silverdal@ga.ntig.se>
2020-10-28 08:03:07 +0100

    Initial commit

---
+++ src/Screen.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+
+/**
+ * This is a class
+ * Created 2020-10-28
+ *
+ * @author Magnus Silverdal
+ */
+public class Screen {
+
+
+}
---
+++ src/Transform.java
@@ -0,0 +1,9 @@
+/**
+ * This is a class
+ * Created 2020-10-28
+ *
+ * @author Magnus Silverdal
+ */
+public class Transform {
+
+}
